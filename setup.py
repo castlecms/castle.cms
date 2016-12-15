68cf4d617b3256bc1758f7ab735ae07cf11ae85d
vangheem <vangheem@gmail.com>
2016-12-14 18:49:51 -0600

    Prepare castle.cms 2.0.2.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(*rnames):
 
 setup(
     name='castle.cms',
-    version='2.0.1',
+    version='2.0.2',
     description='',
     long_description='%s\n%s\n%s' % (
         read('README.rst'),
