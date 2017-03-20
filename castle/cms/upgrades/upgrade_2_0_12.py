f0305736ae5231ff2b709b74edd20a08bdd5451a
vangheem <vangheem@gmail.com>
2017-03-20 10:46:32 -0500

    make sure to cook js/css again

--- castle/cms/upgrades/upgrade_2_0_12.py
+++ castle/cms/upgrades/upgrade_2_0_12.py
@@ -1,4 +1,6 @@
+from plone import api
 from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.resources.browser.cook import cookWhenChangingSettings
 
 
 PROFILE_ID = 'profile-castle.cms.upgrades:2_0_12'
@@ -7,3 +9,4 @@ PROFILE_ID = 'profile-castle.cms.upgrades:2_0_12'
 def upgrade(context, logger=None):
     setup = getToolByName(context, 'portal_setup')
     setup.runAllImportStepsFromProfile(PROFILE_ID)
+    cookWhenChangingSettings(api.portal.get())
