21952a254fdd117af72e54c70b913ce0bf958661
vangheem <vangheem@gmail.com>
2017-04-25 16:32:08 -0500

    Better error pages with stacktrace info if it's possible to provide fixes #62

---
+++ castle/cms/browser/error.py
@@ -0,0 +1,23 @@
+from castle.cms.interfaces import ICastleApplication
+from plone import api
+from Products.Five import BrowserView
+
+
+class ErrorView(BrowserView):
+
+    @property
+    def is_zope_root(self):
+        return ICastleApplication.providedBy(self.context)
+
+    @property
+    def logged_in(self):
+        try:
+            return not api.user.is_anonymous()
+        except:
+            return False
+
+    def format_error_message(self):
+        return repr(self.context)
+
+    def __call__(self):
+        return self.index()
