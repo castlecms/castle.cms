e1f1e5f7b1026154a8c7e7321176edbbb19f36fb
Kyle Arthurs <kylearthurs21@gmail.com>
2018-02-19 12:45:18 -0600

    Clean up comments in code

--- castle/cms/browser/controlpanel/cloudflare.py
+++ castle/cms/browser/controlpanel/cloudflare.py
@@ -1,12 +1,9 @@
 from castle.cms.caching.cloudflare import ICloudflareSettings
 from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
 from plone.app.registry.browser.controlpanel import RegistryEditForm
-#from z3c.form import form
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-#from plone.z3cform import layout
 
 class CloudflareControlPanelForm(RegistryEditForm):
-    #form.extends(RegistryEditForm)
     schema = ICloudflareSettings
     id = "CloudflareControlPanel"
     label = u'Cloudflare Settings'
@@ -16,10 +13,3 @@ class CloudflareControlPanelForm(RegistryEditForm):
 class CloudflareControlPanel(ControlPanelFormWrapper):
     form = CloudflareControlPanelForm
     index = ViewPageTemplateFile('templates/cloudflare.pt')
-
-    # def __init__(self, *args, **kwargs):
-    #     super(CloudflareControlPanelFormWrapper, self).__init__(*args, **kwargs)
-
-# CloudflareControlPanelView = layout.wrap_form(
-#     CloudflareControlPanelForm,
-#     CloudflareControlPanelFormWrapper)
