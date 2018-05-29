5b4dcc6f79a64557c5a6ee3f5cba0d04c1b2dbe8
lucid-0 <kylearthurs21@gmail.com>
2018-05-29 12:26:38 -0500

    Finish custom survey link option. Clean out some old comments.

--- castle/cms/browser/survey.py
+++ castle/cms/browser/survey.py
@@ -2,11 +2,6 @@ from zope.interface import Interface
 from zope import schema
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from z3c.form import form
-from plone.registry.interfaces import IRegistry
-from zope.component import getUtility
-from plone.protect.interfaces import IDisableCSRFProtection
-from zope.interface import alsoProvides
-import requests
 
 class ICastleSurvey(Interface):
     survey_api_url = schema.TextLine(
@@ -28,16 +23,3 @@ class CastleSurvey(form.Form):
     ignoreContext = True
     schema = ICastleSurvey
     template = ViewPageTemplateFile("templates/survey.pt")
-
-    #def __init__(self, context, request):
-    #    super(CastleSurvey, self).__init__(context, request)
-    #    alsoProvides(request, IDisableCSRFProtection)
-    #    registry = getUtility(IRegistry)
-    #    survey_settings = registry.forInterface(ICastleSurvey, check=False)
-        #api_url = survey_settings.survey_api_url
-        #api_key = survey_settings.survey_api_key
-        #data = '''{
-        #}'''
-        #surveys = requests.post(api_url,data=data)
-        #surveys = [u'newurl|New', u'newurl2|Test', u'newurl3|Stuff']
-        #registry['castle.survey_list'] = surveys
