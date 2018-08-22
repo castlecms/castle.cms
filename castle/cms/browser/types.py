645bf062ec5f5e3c54196380d4f47305a538c2e3
Cory Sanin <cory.sanin@wildcardcorp.com>
2018-08-22 09:57:41 -0500

    Hide clone button due to redundancy

--- castle/cms/browser/types.py
+++ castle/cms/browser/types.py
@@ -4,9 +4,16 @@ from plone.app.dexterity.browser import types
 from plone.z3cform import layout
 from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
 
+class TypeEditForm(types.TypeEditForm):
+
+    #Remove clone button
+    def __init__(self, context, request):
+        super(TypeEditForm, self).__init__(context, request)
+        self.buttons = self.buttons.copy().omit('clone')
 
 class TypesListing(types.TypesListing):
     template = ViewPageTemplateFile('templates/types-listing.pt')
+    editform_factory = TypeEditForm
 
 # Create a form wrapper so the form gets layout.
 TypesListingPage = layout.wrap_form(
