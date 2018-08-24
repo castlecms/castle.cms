a24672fafd349ac936accd6ddaaf26e2fd6f2366
Cory Sanin <cory.sanin@wildcardcorp.com>
2018-08-24 10:38:11 -0500

    unnecessary init
    since I'm pushing changes to this branch anyway, might as well remove it. nbd

--- castle/cms/browser/imageinfo.py
+++ castle/cms/browser/imageinfo.py
@@ -4,10 +4,6 @@ from castle.cms import utils
 
 class ImageInfoView(BrowserView):
 
-    def __init__(self, context, request):
-        self.context = context
-        self.request = request
-
     def __call__(self):
         response = self.request.response
         response.setHeader("Content-type", "application/json")
