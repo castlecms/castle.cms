88640dc66ab1aa09324267f4ee0f0275e80eddd3
Sam Schwartz <obct537@gmail.com>
2018-08-30 13:26:24 -0500

    commenting, removed redundant return

--- castle/cms/tiles/querylisting.py
+++ castle/cms/tiles/querylisting.py
@@ -275,9 +275,11 @@ class QueryListingTile(BaseTile, DisplayTypeTileMixin):
             out = json.dumps(config)
         except UnicodeError:
             try:
+                # try to gracefully smooth over any unicode errors
                 out = json.dumps(config, ensure_ascii=False)
             except UnicodeError:
-                return '{}'
+                # It still didn't work. Let's just return an empty object
+                pass
         return out
 
 
