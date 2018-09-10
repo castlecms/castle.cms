c1e59a4e4bfe2cbe0141125a0efe8c93d77ec51c
Cory <coryaccounts@outlook.com>
2018-09-10 12:14:21 -0500

    use castle layer for testing

--- castle/cms/tests/test_keywordmanager.py
+++ castle/cms/tests/test_keywordmanager.py
@@ -1,16 +1,19 @@
+# -*- coding: utf-8 -*-
 from Products.PloneKeywordManager.tests.test_controlpanel import ControlPanelTestCase
 from Products.PloneKeywordManager.tests.test_dexterity import DexterityContentTestCase
 from Products.PloneKeywordManager.tests.test_non_ascii_keywords import NonAsciiKeywordsTestCase
 from Products.PloneKeywordManager.tests.test_setup import InstallTestCase
+from castle.cms.testing import CASTLE_PLONE_INTEGRATION_TESTING
 
 class ControlPanel(ControlPanelTestCase):
-    pass
+    layer = CASTLE_PLONE_INTEGRATION_TESTING
 
 class DexterityContent(DexterityContentTestCase):
-    pass
+    layer = CASTLE_PLONE_INTEGRATION_TESTING
 
 class NonAsciiKeywords(NonAsciiKeywordsTestCase):
+    #layer = CASTLE_PLONE_INTEGRATION_TESTING #fails for some reason?
     pass
 
 class Install(InstallTestCase):
-    pass
+    layer = CASTLE_PLONE_INTEGRATION_TESTING
