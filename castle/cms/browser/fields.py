bbae3b3b2780fa15ab9ee336830f614d033f17c7
Cory <coryaccounts@outlook.com>
2018-10-16 16:15:46 -0500

    don't return widget data for behaviors when saving default values

--- castle/cms/browser/fields.py
+++ castle/cms/browser/fields.py
@@ -2,7 +2,28 @@
 import pkg_resources
 
 from plone.app.dexterity.browser import fields
-from plone.app.dexterity.browser.fields import EnhancedSchemaListing
+from plone.schemaeditor.browser.schema.listing import ReadOnlySchemaListing
+from zope.event import notify
+from z3c.form.events import DataExtractedEvent
+
+
+class EnhancedSchemaListing(fields.EnhancedSchemaListing):
+    def extractData(self, setErrors=True):
+        data, errors = super(EnhancedSchemaListing, self).extractData(setErrors=setErrors)
+        for group in self.groups:
+            groupData, groupErrors = group.extractData(setErrors=setErrors)
+            data.update(groupData)
+            if groupErrors:
+                if errors:
+                    errors += groupErrors
+                else:
+                    errors = groupErrors
+        for fname, value in data.items():
+            if fname not in self.context.schema:
+                del data[fname]
+        notify(DataExtractedEvent(data, errors, self))
+        return data, errors
+
 
 if pkg_resources.get_distribution('plone.resourceeditor'):
     advancedbtns = EnhancedSchemaListing.buttons.copy()
@@ -17,3 +38,10 @@ class TypeFieldsPage(fields.TypeFieldsPage):
                 EnhancedSchemaListing.buttons = advancedbtns
             else:
                 EnhancedSchemaListing.buttons = regularbtns
+
+    @property
+    def form(self):
+        if self.context.fti.hasDynamicSchema:
+            return EnhancedSchemaListing
+        else:
+            return ReadOnlySchemaListing
