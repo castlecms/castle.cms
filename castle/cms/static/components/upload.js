34b829126faf2a619274fe66aa8d22fab0c254b0
rob.porter <robzonenet@gmail.com>
2018-10-30 18:01:43 -0400

    put in better message and new button.

--- castle/cms/static/components/upload.js
+++ castle/cms/static/components/upload.js
@@ -603,6 +603,7 @@ define([
         D.div({ ref: 'droparea', className: 'droparea' }, [
           dropTxt,
           D.button({ ref: 'upload_btn', className: 'btn btn-default castle-btn-select-files' }, 'Select files'),
+          D.p({className: 'upload-text drop-msg'},'Files will be uploaded to central repositories unless you choose folder'),
         ]),
         that.renderAutoApprove(),
         that.renderFileList()
@@ -680,14 +681,14 @@ define([
         ]);
       }else{
         return D.div({ className: 'no-location'}, [
-          D.p({className: 'upload-msg'}, ['Note: By default, the files uploaded to this site are automatically put into a root folder sorted by what kind of file they are. Image/Video/Audio files are put into their respective repositories. Documents and rest of the files are put into a folder called “File repository”. Alternatively, you can also ',  D.a({ href: '#', onClick: function(e){
-              e.preventDefault();
-              var portalUrl = $('body').attr('data-portal-url');
-              var folderUrl = $('body').attr('data-folder-url');
-              that.props.location = folderUrl.replace(portalUrl, '') || '/';
-              that.forceUpdate();
-              that.selectFolderClicked();
-            }}, 'Specify upload location') ]),
+          D.a({ className: 'btn btn-default castle-btn-select-files drop-link', href: '#', onClick: function(e){
+            e.preventDefault();
+            var portalUrl = $('body').attr('data-portal-url');
+            var folderUrl = $('body').attr('data-folder-url');
+            that.props.location = folderUrl.replace(portalUrl, '') || '/';
+            that.forceUpdate();
+            that.selectFolderClicked();
+          }}, 'Choose Folder')
         ]);
       }
     },
