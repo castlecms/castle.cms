5cb86073db990451eefb66cd8335f38e1edc30a2
joel.kleier <joel@kleier.us>
2018-12-04 15:09:26 -0600

    Preparing release 2.3.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*rnames):
 setup(
     name='castle.cms',
     description='CastleCMS Plone distribution main package',
-    version='2.3.2.dev0',
+    version='2.3.2',
     long_description='%s\n%s' % (
         read('README.rst'),
         read('HISTORY.rst')
