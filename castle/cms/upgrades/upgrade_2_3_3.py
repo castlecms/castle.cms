4e9118034f77be446aee34bca9d9c026aa3b2e3c
T. Kim Nguyen <tkn@alumni.uwaterloo.ca>
2018-12-05 13:56:39 -0600

    update upgrade step from 2_0_34 to 2_3_3 after rebase

--- castle/cms/upgrades/upgrade_2_3_3.py
+++ castle/cms/upgrades/upgrade_2_3_3.py
@@ -1,13 +1,10 @@
 from plone import api
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.resources.browser.cook import cookWhenChangingSettings
-from castle.cms import utils
-#from zope.component import getGlobalSiteManager
-#from castle.cms.indexers import recurrence
 import logging
 log = logging.getLogger(__name__)
 
-PROFILE_ID = 'profile-castle.cms.upgrades:2_0_34'
+PROFILE_ID = 'profile-castle.cms.upgrades:2_3_3'
 
 
 def upgrade(context, logger=None):
