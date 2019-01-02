0a47dbc629cac601b0a2ae0aca30e9ed940d4501
joel.kleier <joel@kleier.us>
2019-01-02 12:58:00 -0600

    Back to development: 2.3.8

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*rnames):
 setup(
     name='castle.cms',
     description='CastleCMS Plone distribution main package',
-    version='2.3.7',
+    version='2.3.8.dev0',
     long_description='%s\n%s' % (
         read('README.rst'),
         read('HISTORY.rst')
