e77cd26d5a517d744b5d6ab5fee0e4c85e27572f
rob.porter <robzonenet@gmail.com>
2019-01-16 14:39:16 -0500

    moving over the tooltip stuff from the old try
    Co-authored-by: Wildcard Corp. <corporate@wildcardcorp.com>

--- castle/cms/static/patterns/toolbar/menu-item.js
+++ castle/cms/static/patterns/toolbar/menu-item.js
@@ -44,11 +44,62 @@ define([
       }
       return D.li(
         { className: className},
-        D.a({ href: item.url, onClick: this.onClick, ref: 'a'}, [
+         [D.span({ className: 'simpletooltip_container'}, [D.a({ href: item.url, onClick: this.onClick, ref: 'a', className: 'js-simple-tooltip', 'data-simpletooltip-text': this.props.item.description, 'aria-describedby': 'label_simpletooltip_' + this.props.item.id}, [
           icon,
           this.getLabel()
-        ]));
+        ])], [D.span({ className: 'js-simpletooltip simpletooltip', id: 'label_simpletooltip_' + this.props.item.id, 'aria-hidden': true, 'role': 'tooltip'}, this.props.item.description)])]);
     }
   };
+  
+    // Wait a bit when the complete page is fully loaded
+    // from https://github.com/nico3333fr/jquery-accessible-simple-tooltip-aria
+    $(window).load(function() {
+        //events ------------------
+        $('body')
+            .on('mouseenter focusin', '.js-simple-tooltip', function() {
+                var $this = $(this);
+                var aria_describedby = $this.attr('aria-describedby');
+                var tooltip_to_show_id = aria_describedby;
+                var $tooltip_to_show = $('#' + tooltip_to_show_id);
+                $tooltip_to_show.attr('aria-hidden', 'false');
+            })
+            .on('mouseleave', '.js-simple-tooltip', function(event) {
+                var $this = $(this);
+                var aria_describedby = $this.attr('aria-describedby');
+                var tooltip_to_show_id = aria_describedby;
+                var $tooltip_to_show = $('#' + tooltip_to_show_id);
+                var $is_target_hovered = $tooltip_to_show.is(':hover');
+
+                //alert($target_hovered);
+                //$target.addClass('redborder');
+                if (!$is_target_hovered) {
+                    $tooltip_to_show.attr('aria-hidden', 'true');
+                }
+            })
+            .on('focusout', '.js-simple-tooltip', function(event) {
+                var $this = $(this);
+                var aria_describedby = $this.attr('aria-describedby');
+                var tooltip_to_show_id = aria_describedby;
+                var $tooltip_to_show = $('#' + tooltip_to_show_id);
+
+                $tooltip_to_show.attr('aria-hidden', 'true');
+            })
+            .on('mouseleave', '.js-simpletooltip', function() {
+                var $this = $(this);
+                $this.attr('aria-hidden', 'true');
+            })
+            .on('keydown', '.js-simple-tooltip', function(event) {
+                // close esc key
+
+                var $this = $(this);
+                var aria_describedby = $this.attr('aria-describedby');
+                var tooltip_to_show_id = aria_describedby;
+                var $tooltip_to_show = $('#' + tooltip_to_show_id);
+
+                if (event.keyCode == 27) { // esc
+                    $tooltip_to_show.attr('aria-hidden', 'true');
+                }
+            });
+    });
   return MenuItemBase;
 });
