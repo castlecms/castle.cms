898dbba906e6d2deb15470188d72871005f61c00
joel.kleier <joel@kleier.us>
2019-01-28 13:33:16 -0600

    Back to development: 2.4.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     name='castle.cms',
     description='CastleCMS Plone distribution main package',
     long_description_content_type='text/x-rst',
-    version='2.4.0',
+    version='2.4.1.dev0',
     long_description='%s\n%s' % (
         read('README.rst'),
         read('HISTORY.rst')
