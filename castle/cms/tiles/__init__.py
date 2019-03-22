8d692ee137c540baba478ad7c722f0745d3a52c1
vangheem <vangheem@gmail.com>
2019-03-22 10:15:15 -0400

    more fixes

--- castle/cms/tiles/__init__.py
+++ castle/cms/tiles/__init__.py
@@ -6,9 +6,11 @@ from castle.cms.interfaces import IVersionViewLayer
 from plone import api
 from plone.app.standardtiles.contentlisting import ContentListingTile
 from plone.resource.interfaces import IResourceDirectory
+from plone.tiles.data import defaultPersistentTileDataStorage
 from plone.tiles.interfaces import IPersistentTile
 from plone.tiles.interfaces import ITile
 from plone.tiles.interfaces import ITileDataContext
+from plone.tiles.interfaces import ITileDataStorage
 from zope.annotation.interfaces import IAnnotations
 from zope.component import adapter
 from zope.interface import Interface
@@ -57,3 +59,13 @@ def ResourceDirectoryAnnotations(context):
     # storage. We should just proxy to the annotations on the site here
     # This fixes some errors when previewing the theme from the layout editor
     return IAnnotations(api.portal.get())
+
+
+@implementer(ITileDataStorage)
+@adapter(Interface, ICastleLayer, ITile)
+def TileDataStorage(context, request, tile):
+    if (tile.request.get('X-Tile-Persistent') or
+            getattr(request, 'tile_persistent', False)):
+        return defaultPersistentTileDataStorage(context, request, tile)
+    else:
+        return IAnnotations(tile.request, tile.request.form)
