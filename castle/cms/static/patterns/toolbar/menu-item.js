bd524ef19510be927ae8cfd685b9b3f3b536ca06
vangheem <vangheem@gmail.com>
2019-03-25 12:26:27 -0400

    change to only use react

--- castle/cms/static/patterns/toolbar/menu-item.js
+++ castle/cms/static/patterns/toolbar/menu-item.js
@@ -19,6 +19,12 @@ define([
         showIcon: true
       };
     },
+    getInitialState: function(){
+      return {
+        hovered: false,
+        focussed: false
+      }
+    },
     onClick: undefined,
     getLabel: function(){
       return this.props.item.title;
@@ -43,63 +49,49 @@ define([
         icon = D.span({ className: item.icon_class, 'aria-hidden': true });
       }
       return D.li(
-        { className: className},
-         [D.span({ className: 'simpletooltip_container'}, [D.a({ href: item.url, onClick: this.onClick, ref: 'a', className: 'js-simple-tooltip', 'data-simpletooltip-text': this.props.item.description, 'aria-describedby': 'label_simpletooltip_' + this.props.item.id}, [
-          icon,
-          this.getLabel()
-        ])], [D.span({ className: 'js-simpletooltip simpletooltip', id: 'label_simpletooltip_' + this.props.item.id, 'aria-hidden': true, 'role': 'tooltip'}, this.props.item.description)])]);
+        { className: className}, [
+          D.span({ className: 'simpletooltip_container'}, [
+            D.a({ href: item.url, onClick: this.onClick,
+                  ref: 'a', className: 'js-simple-tooltip',
+                  onFocus: function(){
+                    this.setState({
+                      focussed: true
+                    });
+                  }.bind(this),
+                  onMouseEnter: function(){
+                    this.setState({
+                      hovered: true
+                    });
+                  }.bind(this),
+                  onBlur: function(){
+                    this.setState({
+                      focussed: false
+                    });
+                  }.bind(this),
+                  onMouseLeave: function(){
+                    this.setState({
+                      hovered: false
+                    });
+                  }.bind(this),
+                  onKeyDown: function(){
+                    if (event.keyCode == 27) { // esc
+                      this.setState({
+                        focussed: false
+                      });
+                    }
+                  }.bind(this),
+                  'aria-describedby': 'label_simpletooltip_' + this.props.item.id}, [
+            icon,
+            this.getLabel()
+          ]),
+          D.span({ className: 'js-simpletooltip simpletooltip',
+                   id: 'label_simpletooltip_' + this.props.item.id,
+                   'aria-hidden': !this.state.focussed && !this.state.hovered,
+                   'role': 'tooltip'},
+            this.props.item.description)
+          ])
+      ]);
     }
   };
-  
-    // Wait a bit when the complete page is fully loaded
-    // from https://github.com/nico3333fr/jquery-accessible-simple-tooltip-aria
-    $(window).load(function() {
-        //events ------------------
-        $('body')
-            .on('mouseenter focusin', '.js-simple-tooltip', function() {
-                var $this = $(this);
-                var aria_describedby = $this.attr('aria-describedby');
-                var tooltip_to_show_id = aria_describedby;
-                var $tooltip_to_show = $('#' + tooltip_to_show_id);
-                $tooltip_to_show.attr('aria-hidden', 'false');
-            })
-            .on('mouseleave', '.js-simple-tooltip', function(event) {
-                var $this = $(this);
-                var aria_describedby = $this.attr('aria-describedby');
-                var tooltip_to_show_id = aria_describedby;
-                var $tooltip_to_show = $('#' + tooltip_to_show_id);
-                var $is_target_hovered = $tooltip_to_show.is(':hover');
-
-                //alert($target_hovered);
-                //$target.addClass('redborder');
-                if (!$is_target_hovered) {
-                    $tooltip_to_show.attr('aria-hidden', 'true');
-                }
-            })
-            .on('focusout', '.js-simple-tooltip', function(event) {
-                var $this = $(this);
-                var aria_describedby = $this.attr('aria-describedby');
-                var tooltip_to_show_id = aria_describedby;
-                var $tooltip_to_show = $('#' + tooltip_to_show_id);
-
-                $tooltip_to_show.attr('aria-hidden', 'true');
-            })
-            .on('mouseleave', '.js-simpletooltip', function() {
-                var $this = $(this);
-                $this.attr('aria-hidden', 'true');
-            })
-            .on('keydown', '.js-simple-tooltip', function(event) {
-                // close esc key
-
-                var $this = $(this);
-                var aria_describedby = $this.attr('aria-describedby');
-                var tooltip_to_show_id = aria_describedby;
-                var $tooltip_to_show = $('#' + tooltip_to_show_id);
-
-                if (event.keyCode == 27) { // esc
-                    $tooltip_to_show.attr('aria-hidden', 'true');
-                }
-            });
-    });
   return MenuItemBase;
 });
