3af219a87cef662969b5bb4d51da59e6591c5460
lucid-0 <kylearthurs21@gmail.com>
2019-04-25 11:34:46 -0500

    Fix richtext import when exporting from old plone, add optional interval between content

--- castle/cms/_scripts/export-content.py
+++ castle/cms/_scripts/export-content.py
@@ -2,6 +2,7 @@ import argparse
 import base64
 import errno
 import logging
+import time
 import os
 import re
 from datetime import datetime
@@ -23,7 +24,6 @@ from persistent.list import PersistentList
 from persistent.mapping import PersistentMapping as PM2  # noqa
 from plone.app.blob.field import BlobWrapper
 from plone.app.blob.utils import openBlob
-from plone.app.textfield import RichText
 from Products.Archetypes import Field
 from Products.CMFCore.interfaces import ISiteRoot
 from Products.CMFCore.tests.base.security import OmnipotentUser
@@ -100,8 +100,10 @@ except ImportError:
     NamedBlobImage = object()
 
 try:
+    from plone.app.textfield import RichText
     from plone.app.textfield.value import RichTextValue
 except ImportError:
+    RichText = None
     RichTextValue = None
 
 
@@ -111,6 +113,7 @@ parser.add_argument('--overwrite', dest='overwrite', default=False)
 parser.add_argument('--admin-user', dest='admin_user', default='admin')
 parser.add_argument('--site-id', dest='site_id', default='Plone')
 parser.add_argument('--dir', dest='dir', default='./export')
+parser.add_argument('--interval', dest='interval', default=0)
 parser.add_argument('--modifiedsince', dest='modifiedsince')
 parser.add_argument('--createdsince', dest='createdsince')
 parser.add_argument(
@@ -712,6 +715,11 @@ def export_dexterity_obj(obj):
 
 
 def export_obj(obj):
+    try:
+        interval = int(args.interval)
+        time.sleep(interval)
+    except Exception:
+        pass
     if IDexterityContent.providedBy(obj):
         print("--> Dexterity: %s" % obj.Title())
         func = export_dexterity_obj
