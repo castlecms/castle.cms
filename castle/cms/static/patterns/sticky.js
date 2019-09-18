64536fa6c3c8b1b337c771d96ebc11f52aec948a
lucid-0 <kylearthurs21@gmail.com>
2019-09-18 15:45:20 -0500

    more on template, uuid to URL for internal content

--- castle/cms/static/patterns/sticky.js
+++ castle/cms/static/patterns/sticky.js
@@ -13,7 +13,6 @@ define([
     defaults: {},
     init: function() {
       window.addEventListener("load", event => {
-        debugger;
         $(".sticky-footer").show();
         $(".sticky-footer")
           .css({ bottom: "-100px" })
