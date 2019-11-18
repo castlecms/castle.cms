34a7ab1de694df6bac17070c1e2e5a0e6da0c79a
lucid-0 <kylearthurs21@gmail.com>
2019-11-18 13:48:10 -0600

    fix upgrade, don't setup_site twice

--- castle/cms/upgrades/upgrade_2_5_17.py
+++ castle/cms/upgrades/upgrade_2_5_17.py
@@ -1,12 +1,10 @@
 from castle.cms.cron._pw_expiry import update_password_expiry
-from castle.cms.cron.utils import setup_site
 from plone import api
 
 
 def upgrade(context, logger=None):
-    setup_site(context)
     auth_step_timeout = api.portal.get_registry_record('plone.auth_step_timeout', default=None)
     if not auth_step_timeout:
         api.portal.set_registry_record('plone.auth_step_timeout', 120)
 
-    update_password_expiry(context)
+    update_password_expiry(api.portal.get())
