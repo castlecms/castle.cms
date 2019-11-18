b39a87618f1a7e213a42df69f55b31b7c8715ea1
lucid-0 <kylearthurs21@gmail.com>
2019-11-18 15:02:59 -0600

    upgrade step..

--- castle/cms/upgrades/upgrade_2_5_17.py
+++ castle/cms/upgrades/upgrade_2_5_17.py
@@ -3,8 +3,8 @@ from plone import api
 
 
 def upgrade(context, logger=None):
-    auth_step_timeout = api.portal.get_registry_record('plone.auth_step_timeout', default=None)
+    auth_step_timeout = api.portal.get_registry_record(name='plone.auth_step_timeout', default=None)
     if not auth_step_timeout:
-        api.portal.set_registry_record('plone.auth_step_timeout', 120)
+        api.portal.set_registry_record(name='plone.auth_step_timeout', value=120)
 
     update_password_expiry(api.portal.get())
