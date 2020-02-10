5ed83b663aec1fafa8e051604d7f4931a99b58a7
alphaomega325 <alphaomega325@gmail.com>
2020-02-10 16:21:12 -0600

    added the javascript file

---
+++ castle/cms/static/patterns/trash_checkbox.js
@@ -0,0 +1,46 @@
+define([
+    'jquery',
+    'pat-base'
+    
+], function($, Base) {
+    "use strict";
+
+    var Trash_Checkboxes = Base.extend({
+	name: 'trash_checkbox',
+	trigger: '.pat-trash_checkbox',
+	defaults: {
+	},
+	parser: 'mockup',
+	
+	init: function(){
+	    $("#restore_all").click(function(){
+		
+		
+		var elements = document.getElementsByClassName("restore-checkbox");
+		var restore_item_uid = [];
+		for(var element = 0; element < elements.length; element++)
+		{
+		    console.log(elements[element]);
+		    if(elements[element][1].checked == true)
+		    {
+			console.log(elements[element][0].value);
+			restore_item_uid.push(elements[element][0].value);
+		    };
+		};
+		var form = document.getElementById("restore_all");
+		var input = document.createElement('input');
+		input.setAttribute('name', 'restore_item_uid');
+		input.setAttribute('value', restore_item_uid);
+		input.setAttribute('type', 'hidden');
+
+		form.appendChild(input);
+		form.submit();
+	    });
+	}
+    });
+    
+    return Trash_Checkboxes;
+});
+
+
+				      
