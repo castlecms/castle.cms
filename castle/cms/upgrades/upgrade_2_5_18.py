b8d71141dfaeacd1242dcab4160a49bcb2e59c8b
lucid-0 <kylearthurs21@gmail.com>
2020-02-20 11:45:20 -0600

    register upgrade step, only import profile for upgrade

--- castle/cms/upgrades/upgrade_2_5_18.py
+++ castle/cms/upgrades/upgrade_2_5_18.py
@@ -1,10 +1,4 @@
-from castle.cms.cron._pw_expiry import update_password_expiry
-from plone.registry.interfaces import IRegistry
-from plone.registry import field
-from plone.registry import Record
-from plone import api
 from Products.CMFCore.utils import getToolByName
-from zope.component import getUtility
 
 PROFILE_ID = 'profile-castle.cms:2_5_18'
 
@@ -12,5 +6,3 @@ PROFILE_ID = 'profile-castle.cms:2_5_18'
 def upgrade(context, logger=None):
     setup = getToolByName(context, 'portal_setup')
     setup.runAllImportStepsFromProfile(PROFILE_ID)
-    # need to registry the dexterity type and slide type (profile)
-    registry = getUtility(IRegistry)
