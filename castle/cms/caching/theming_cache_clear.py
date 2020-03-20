36ad67a9724b76cb42bd41e7b7517f6dab7ef6fa
alphaomega325 <alphaomega325@gmail.com>
2020-03-20 16:56:18 -0500

    Getting zcml to recognize my file continues

--- castle/cms/caching/theming_cache_clear.py
+++ castle/cms/caching/theming_cache_clear.py
@@ -1,14 +1,21 @@
 from castle.cms.cache import get_client
 from castle.cms.cache import ram
-from ZODB.Connection.Connection import cacheMinimize
+from plone.app.theming.policy import ThemingPolicy
+from plone.app.theming.interfaces import IThemingPolicy
+from zope.interface import implementer
+#from ZODB.Connection.Connection import cacheMinimize
 
-@implementer(ThemingPolicy)
+@implementer(IThemingPolicy)
 class CastleCmsThemingCacheReset(object):
 
+    def invalidateCache(self):
+        import pdb; pdb.set_trace()
+    
     def _reset_local_cache(self):
-        # Clears out the redis cache, the ramcache.
-        # And ZODB cache as well
+        """
+         Clears out the redis cache, the ramcache.
+         And ZODB cache as well
+        """
         import pdb; pdb.set_trace()
         redis = get_client()
-            
-        pass
+#        cacheMinimize()
