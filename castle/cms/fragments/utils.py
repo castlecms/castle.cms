6b4075e9adbfd372a9148d07104464e70a3e690f
lucid-0 <kylearthurs21@gmail.com>
2020-04-21 10:41:16 -0500

    no error when missing fragment, render warning comment to dom

--- castle/cms/fragments/utils.py
+++ castle/cms/fragments/utils.py
@@ -1,5 +1,12 @@
 from zope.component import getAllUtilitiesRegisteredFor
 from castle.cms.fragments.interfaces import IFragmentsDirectory
+from castle.cms.fragments import FragmentView
+from Products.PageTemplates.ZopePageTemplate import ZopePageTemplate
+
+import logging
+
+
+logger = logging.getLogger('castle.cms')
 
 
 def getFragment(context, request, name):
@@ -13,4 +20,7 @@ def getFragment(context, request, name):
             return util.get(context, request, name)
         except KeyError:
             pass
-    raise KeyError(name)
+    logger.warn('Could not get fragment {0}'.format(name))
+    template = ZopePageTemplate('missing',
+                                text='<!-- fragment with missing template -->')
+    return FragmentView(context, request, name, 'View', template)
