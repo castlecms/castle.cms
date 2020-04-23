cc9cd2458ecf713a039c9132a0b85553fe6d65d5
alphaomega325 <alphaomega325@gmail.com>
2020-04-23 13:42:17 -0500

    Reverted implementation due to lockinfo being already implemented

--- castle/cms/browser/viewlets/lockinfo.py
+++
@@ -1,6 +0,0 @@
-from plone.locking.browser.info import LockInfoViewlet
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
-class CastleCMSLockInfo(LockInfoViewlet):
-    
-    template = ViewPageTemplateFile("../templates/info.pt")
