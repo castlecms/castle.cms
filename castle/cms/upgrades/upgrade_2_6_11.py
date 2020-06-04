ceac23f1489baf9afee134eebbc706941c866452
lucid-0 <kylearthurs21@gmail.com>
2020-06-04 12:34:16 -0500

    traverse all for reindex

--- castle/cms/upgrades/upgrade_2_6_11.py
+++ castle/cms/upgrades/upgrade_2_6_11.py
@@ -8,6 +8,10 @@ def upgrade(site, logger=None):
     setup = getToolByName(site, 'portal_setup')
     setup.runAllImportStepsFromProfile(PROFILE_ID)
     cookWhenChangingSettings(site)
+    recursive_index(site)
 
-    for id in site.objectIds():
-        site[id].reindexObject(idxs=['self_or_child_has_title_description_and_image'])
+def recursive_index(obj):
+        if not IPloneSiteRoot.providedBy(obj):
+                obj.reindexObject(idxs=['self_or_child_has_title_description_and_image'])
+        for childID in obj.objectIds():
+                recursive_index(obj[ChildID])
