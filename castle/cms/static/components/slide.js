f1f54e4de750061f6e973f37c57918cdb9135c95
bduncan137 <brian.duncan@wildcardcorp.com>
2020-07-14 20:39:21 -0500

    slide.js-hide and reveal mobile alignment options (#448)

--- castle/cms/static/components/slide.js
+++ castle/cms/static/components/slide.js
@@ -36,7 +36,7 @@ require(['jquery'], function slideComponent($) {
     }
     return formVariables;
   }
-  
+
   function hasId(element) {
     return !!element.id;
   }
@@ -46,14 +46,9 @@ require(['jquery'], function slideComponent($) {
     const imageSlidetypes = ['background-image', 'left-image-right-text'];
     const videoSlidetypes = ['background-video', 'left-video-right-text'];
     const backgroundSlidetypes = ['background-image', 'background-video'];
+    const sideSlideTypes = ['left-image-right-text', 'left-video-right-text'];
     const mediaSlidetypes = [...imageSlidetypes, ...videoSlidetypes];
-    const fieldsetLabels = [
-      'Type & Text',
-      'Text Positioning',
-      'Mobile Text Positioning',
-      'Media Settings',
-      'Related Items',
-    ];
+    const fieldsetLabels = ['Type & Text', 'Text Positioning', 'Media Settings', 'Related Items'];
     if (imageSlidetypes.includes(slideType)) {
       showAndHide.fieldIdEndingsToHide = ['widgets-video', 'related_items'];
     }
@@ -67,29 +62,34 @@ require(['jquery'], function slideComponent($) {
         'widgets-title',
         'widgets-text',
         'hor_text_position',
-        'vert_text_position'
+        'vert_text_position',
       ];
     }
     if (mediaSlidetypes.includes(slideType)) {
-      showAndHide.hideFieldsetsWithLabels = ['Related Items'];
-      showAndHide.unhideFieldsetsWithLabels = fieldsetLabels.filter(label => label !== 'Related Items');
-    } else {
-      const hideLabels = ['Text Positioning', 'Mobile Text Positioning', 'Media Settings'];
+      const hideLabels = ['Related Items'];
       showAndHide.hideFieldsetsWithLabels = hideLabels;
-      showAndHide.unhideFieldsetsWithLabels = fieldsetLabels.filter(label => !hideLabels.includes(label));
+      showAndHide.unhideFieldsetsWithLabels = fieldsetLabels.filter((label) => !hideLabels.includes(label));
+    } else {
+      const hideLabels = ['Text Positioning', 'Media Settings'];
+      showAndHide.hideFieldsetsWithLabels = hideLabels;
+      showAndHide.unhideFieldsetsWithLabels = fieldsetLabels.filter((label) => !hideLabels.includes(label));
     }
     if (backgroundSlidetypes.includes(slideType)) {
       showAndHide.fieldIdEndingsToHide.push('widgets-customize_left_slide_mobile');
       showAndHide.hideFieldsetsWithLabels.push('Mobile Text Positioning');
-      showAndHide.unhideFieldsetsWithLabels = fieldsetLabels.filter(label => label !== 'Mobile Text Positioning');
+    } else if (sideSlideTypes.includes(slideType)) {
+      if ($('#form-widgets-customize_left_slide_mobile-0')[0].checked) {
+        showAndHide.unhideFieldsetsWithLabels.push('Mobile Text Positioning');
+      } else {
+        showAndHide.hideFieldsetsWithLabels.push('Mobile Text Positioning');
+      }
     }
     return showAndHide;
   }
 
   function shouldFieldBeDisplayed(id, idEndingsToHide) {
-    return !idEndingsToHide.some(ending => id.endsWith(ending));
+    return !idEndingsToHide.some((ending) => id.endsWith(ending));
   }
-
 
   function showAndHideFieldsAndFieldsets(form) {
     const { slideType, formFields } = form;
@@ -111,25 +111,41 @@ require(['jquery'], function slideComponent($) {
 
   function _showAndHideFieldsets(hideFieldsetsWithLabels, unhideFieldsetsWithLabels) {
     potentialFormTabs = Array.from(document.querySelectorAll('nav>a'));
-    potentialFormTabs.forEach(tab => {
+    potentialFormTabs.forEach((tab) => {
       if (hideFieldsetsWithLabels.includes(tab.textContent)) {
         tab.setAttribute('style', 'display: none;');
-      }
-      else if (unhideFieldsetsWithLabels.includes(tab.textContent)) {
+      } else if (unhideFieldsetsWithLabels.includes(tab.textContent)) {
         tab.removeAttribute('style');
       }
     });
   }
 
+  function _showAndHideSpecificFieldset(fieldsetText, show) {
+    potentialFormTabs = Array.from(document.querySelectorAll('nav>a'));
+    potentialFormTabs.some((tab) => {
+      if (tab.textContent === fieldsetText) {
+        if (show) {
+          tab.removeAttribute('style');
+        } else {
+          tab.setAttribute('style', 'display: none;');
+        }
+        return true;
+      }
+      return false;
+    });
+  }
+
   function modifyAddEditTile(mutations) {
-    mutations.some(mutation => {
-      const tileAdded = Array.from(mutation.addedNodes).some(addedNode => {
+    mutations.some((mutation) => {
+      const tileAdded = Array.from(mutation.addedNodes).some((addedNode) => {
         try {
           const form = addedNode.querySelector('#edit_tile') || addedNode.querySelector('#add_tile');
           if (form) {
             formVariables = getFormVariables(null, form);
             showAndHideFieldsAndFieldsets(formVariables);
             $('#form-widgets-display_type').change(onSlideTypeChange);
+            toggleMobileTextPositionFieldset();
+            $('#form-widgets-customize_left_slide_mobile-0').change(toggleMobileTextPositionFieldset);
           }
           return !!form;
         } catch (err) {
@@ -143,7 +159,11 @@ require(['jquery'], function slideComponent($) {
   function observe() {
     const observer = new MutationObserver(modifyAddEditTile);
     const body = document.querySelector('body');
-    const observerOptions = { childList: true, attributes: false, subtree: true };
+    const observerOptions = {
+      childList: true,
+      attributes: false,
+      subtree: true,
+    };
     observer.observe(body, observerOptions);
   }
 
@@ -151,9 +171,15 @@ require(['jquery'], function slideComponent($) {
     const form = getFormVariables(event);
     showAndHideFieldsAndFieldsets(form);
   }
-  
+
+  function toggleMobileTextPositionFieldset() {
+    const showMobile = $('#form-widgets-customize_left_slide_mobile-0')[0].checked;
+    _showAndHideSpecificFieldset('Mobile Text Positioning', showMobile);
+  }
+
   if (!window.slideScriptCalled) {
     window.slideScriptCalled = true;
     $('document').ready(observe);
   }
+  toggleMobileTextPositionFieldset;
 });
