7c1586c3dddf7b5bac5dd8e1a175779fc0851d1f
David Henne <David@David-Hs-MacBook-Air.local>
2020-08-10 15:33:48 -0500

    Content excluded from search will show up in Config Registry Record

--- castle/cms/subscribers.py
+++ castle/cms/subscribers.py
@@ -15,9 +15,11 @@ from Products.CMFCore.interfaces._content import IFolderish
 from Products.CMFCore.WorkflowCore import WorkflowException
 from Products.CMFPlone.browser.syndication.settings import FeedSettings
 from zope.component import getUtility
+from zope.component.interfaces import ComponentLookupError
 from zope.globalrequest import getRequest
 from zope.interface import Interface
 import logging
+
 
 logger = logging.getLogger('castle.cms')
 
@@ -208,8 +210,22 @@ def on_youtube_video_state_changed(obj, event):
 
 def on_search_exclusion(obj, event):
     try:
+        registry_list = api.portal.get_registry_record('excluded_from_search')
+    except ComponentLookupError:
+        return
+
+    try:
         if obj.exclude_from_search:
-            # Add to list of excluded content somewhere on control panel
-            import pdb; pdb.set_trace()
+            if obj.id not in registry_list:
+                registry_list.append(obj.id)
+        else:
+            if obj.id in registry_list:
+                registry_list.remove(obj.id)
     except AttributeError:
-        pass
+        return
+
+    try:
+        api.portal.set_registry_record(name='excluded_from_search', value=registry_list)
+    except:
+        return
+
