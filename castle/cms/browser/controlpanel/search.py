fd53f78895173f02edd6e93dddd5a01afe6ade71
lucid-0 <kylearthurs21@gmail.com>
2020-09-15 13:13:24 -0500

    list of published items with private parents (brian's amazing search link)

--- castle/cms/browser/controlpanel/search.py
+++ castle/cms/browser/controlpanel/search.py
@@ -7,3 +7,9 @@ class ExclusionPanel(BrowserView):
         catalog = api.portal.get_tool('portal_catalog')
         excluded_content = catalog(exclude_from_search=True)
         return excluded_content
+
+class PublishedWithPrivateParents(BrowserView):
+    def get_published_private_parent_content(self):
+        catalog = api.portal.get_tool('portal_catalog')
+        content = catalog(review_state='published', has_private_parents=True)
+        return content
