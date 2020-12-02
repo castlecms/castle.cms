afa5bb253dc337a2a17415a524a5488f3c88d9b8
Brian Duncan <brian.duncan@wildcardcorp.com>
2020-12-02 14:00:32 -0600

    flake8 changes

--- castle/cms/behaviors/search.py
+++ castle/cms/behaviors/search.py
@@ -4,7 +4,6 @@ from plone.autoform.interfaces import IFormFieldProvider
 from plone.supermodel import model
 from zope import schema
 from zope.interface import alsoProvides
-from zope.schema.vocabulary import SimpleVocabulary
 
 
 class ISearch(model.Schema):
