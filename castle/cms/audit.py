15b40c9fa0cee59d33469e619a7c4c108c487c31
Brian Duncan <brian.duncan@wildcardcorp.com>
2021-02-01 09:32:31 -0600

    fix possible ComponentLookupError in get_index_name

--- castle/cms/audit.py
+++ castle/cms/audit.py
@@ -169,13 +169,14 @@ def _create_index(es, index_name):
 def get_index_name(site_path=None):
     if site_path is None:
         site_path = '/'.join(api.portal.get().getPhysicalPath())
-
-    es_index_enabled = api.portal.get_registry_record('castle.es_index_enabled', default=False)
-    if es_index_enabled:
-        new_index = api.portal.get_registry_record('castle.es_index')
-        return new_index + '-audit'
-    else:
-        return site_path.replace('/', '').replace(' ', '').lower()
+    try:
+        es_index_enabled = api.portal.get_registry_record('castle.es_index_enabled', default=False)
+        if es_index_enabled:
+            new_index = api.portal.get_registry_record('castle.es_index')
+            return new_index + '-audit'
+    except ComponentLookupError:
+        pass
+    return site_path.replace('/', '').replace(' ', '').lower()
 
 
 def _record(conn_factory, site_path, data):
