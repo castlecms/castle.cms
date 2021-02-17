01a63cdcfbcbcec6a5dd761a849f748943c831b6
David Henne <David@David-Hs-MacBook-Air.local>
2021-02-17 10:36:50 -0600

    Custom versionable behavior requires change note on content type edit.

---
+++ castle/cms/behaviors/versionable.py
@@ -0,0 +1,102 @@
+from plone.app.versioningbehavior import _
+from plone.autoform import directives as form
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.dexterity.interfaces import IDexterityContent
+from plone.supermodel import model
+from z3c.form.interfaces import IAddForm
+from z3c.form.interfaces import IEditForm
+from zope import schema
+from zope.annotation.interfaces import IAnnotations
+from zope.component import adapter
+from zope.interface import alsoProvides
+from zope.interface import implementer
+from zope.interface import Interface
+from zope.publisher.interfaces.browser import IBrowserRequest
+
+
+class IVersionable(model.Schema):
+    """ Behavior for enabling CMFEditions's versioning for dexterity
+    content types. Be shure to enable versioning in the plone types
+    control-panel for your content type.
+    """
+
+    model.fieldset(
+        'settings',
+        label=_(u'Settings'),
+        fields=['versioning_enabled']
+    )
+    changeNote = schema.TextLine(
+        title=_(u'label_change_note', default=u'Change Note'),
+        description=_(u'help_change_note',
+                      default=u'Enter a comment that describes the changes you made. (May be under properties tab)'),
+        required=True)
+
+    versioning_enabled = schema.Bool(
+        title=_(u'label_versioning_enabled', default=u'Versioning enabled'),
+        description=_(u'help_versioning_enabled',
+                      default=u'Enable/disable versioning for this document.'),
+        default=True,
+        required=False)
+
+    form.order_after(changeNote='*')
+    form.omitted('changeNote')
+    form.no_omit(IEditForm, 'changeNote')
+    form.no_omit(IAddForm, 'changeNote')
+
+alsoProvides(IVersionable, IFormFieldProvider)
+
+
+class IVersioningSupport(Interface):
+    """
+    Marker Interface for the IVersionable behavior.
+    """
+
+
+@implementer(IVersionable)
+@adapter(IDexterityContent)
+class Versionable(object):
+    """ The Versionable adapter prohibits dexterity from saving the changeNote
+    on the context. It stores it in a request-annotation for later use in
+    event-handlers
+    The versioning_enabled flag is stored at the context itself.
+    """
+
+    def __init__(self, context):
+        self.context = context
+
+    @property
+    def changeNote(self):
+        return ''
+
+    @changeNote.setter
+    def changeNote(self, value):
+        # store the value for later use (see events.py)
+        annotation = IAnnotations(self.context.REQUEST)
+        annotation['plone.app.versioningbehavior-changeNote'] = value
+
+    @property
+    def versioning_enabled(self):
+        return self.context.versioning_enabled
+
+    @versioning_enabled.setter
+    def versioning_enabled(self, value):
+        setattr(self.context, 'versioning_enabled', value)
+
+
+def get_change_note(request, default=None):
+    """Returns the changeNote submitted with this request. The changeNote
+    is read from the form-field
+    """
+
+    _marker = object()
+
+    value = _marker
+    if IBrowserRequest.providedBy(request):
+        annotations = IAnnotations(request)
+        value = annotations.get(
+            'plone.app.versioningbehavior-changeNote', _marker)
+
+    if not value or value == _marker:
+        return default
+
+    return value
--- castle/cms/subscribers.py
+++ castle/cms/subscribers.py
@@ -1,5 +1,6 @@
 from castle.cms import audit
 from castle.cms import tasks
+from castle.cms.behaviors.versionable import get_change_note
 from castle.cms.constants import DEFAULT_SITE_LAYOUT_REGISTRY_KEY
 from castle.cms.lead import check_lead_image
 from plone import api
@@ -9,12 +10,18 @@ from plone.app.blocks.layoutbehavior import ILayoutAware
 from plone.app.dexterity.behaviors.metadata import IOwnership
 from plone.app.dexterity.behaviors.metadata import IPublication
 from plone.app.event.base import localized_now
+from plone.app.versioningbehavior import _
 from plone.registry.interfaces import IRegistry
 from plone.uuid.interfaces import IUUID
 from Products.CMFCore.interfaces._content import IFolderish
+from Products.CMFCore.utils import getToolByName
 from Products.CMFCore.WorkflowCore import WorkflowException
+from Products.CMFEditions.interfaces.IArchivist import ArchivistUnregisteredError
+from Products.CMFEditions.interfaces.IModifier import FileTooLargeToVersionError
 from Products.CMFPlone.browser.syndication.settings import FeedSettings
+from Products.CMFPlone.utils import base_hasattr
 from zope.component import getUtility
+from zope.container.interfaces import IContainerModifiedEvent
 from zope.globalrequest import getRequest
 from zope.interface import Interface
 import logging
@@ -209,3 +216,110 @@ def on_youtube_video_state_changed(obj, event):
             tasks.youtube_video_state_changed.delay(obj)
         except CannotGetPortalError:
             pass
+
+def create_version_on_save(context, event):
+    """Creates a new version on a versionable object when the object is saved.
+    A new version is created if the type is automatic versionable and has
+    changed or if the user has entered a change note.
+    """
+    # according to Products.CMFEditions' update_version_on_edit script
+
+    # only version the modified object, not its container on modification
+    if IContainerModifiedEvent.providedBy(event):
+        return
+
+    # XXX dirty hack for stagingbehavior, which triggers a event with
+    # a aq_based context when deleting the working copy
+    try:
+        pr = context.portal_repository
+    except AttributeError:
+        return
+
+    if not pr.isVersionable(context):
+        # cancel, the object is not versionable
+        return
+
+    create_version = False
+
+    if getattr(context, 'REQUEST', None):
+        changeNote = get_change_note(context.REQUEST, None)
+    else:
+        changeNote = None
+
+    if changeNote:
+        # user has entered a change note. create a new version even if nothing
+        # has changed.
+        create_version = True
+
+    elif pr.supportsPolicy(context, 'at_edit_autoversion'):
+        # automatic versioning is enabled for this portal type
+
+        if not base_hasattr(context, 'version_id'):
+            # we do not have a initial version
+            create_version = True
+        else:
+            try:
+                create_version = not pr.isUpToDate(context, context.version_id)
+            except ArchivistUnregisteredError:
+                # The object is not actually registered, but a version is
+                # set, perhaps it was imported, or versioning info was
+                # inappropriately destroyed
+                create_version = True
+
+    # create new version if needed
+    if create_version:
+        pr.save(obj=context, comment=changeNote)
+
+
+def create_initial_version_after_adding(context, event):
+    """Creates a initial version on a object which is added to a container
+    and may be just created.
+    The initial version is created if the content type is versionable,
+    automatic versioning is enabled for this type and there is no initial
+    version. If a changeNote was entered it's used as comment.
+    """
+
+    pr = getToolByName(context, 'portal_repository', None)
+    if pr is None:
+        # This can happen, e.g., when adding a Plone Site with versioning
+        # and portal_repository is not yet created
+        return
+
+    if not pr.isVersionable(context):
+        # object is not versionable
+        return
+
+    if not pr.supportsPolicy(context, 'at_edit_autoversion'):
+        # automatic versioning disabled for this portal type, so we don't
+        # need to create an initial version
+        return
+
+    # get the change not
+    default_changeNote = _(u'initial_version_changeNote',
+                           default=u'Initial version')
+    if getattr(context, 'REQUEST', None):
+        changeNote = get_change_note(context.REQUEST, default_changeNote)
+    else:
+        changeNote = None
+
+    changed = False
+    if not base_hasattr(context, 'version_id'):
+        # no initial version, let's create one..
+        changed = True
+
+    else:
+        try:
+            changed = not pr.isUpToDate(context, context.version_id)
+        except ArchivistUnregisteredError:
+            # The object is not actually registered, but a version is
+            # set, perhaps it was imported, or versioning info was
+            # inappropriately destroyed
+            changed = True
+
+    if not changed:
+        return
+
+    try:
+        context.portal_repository.save(obj=context, comment=changeNote)
+    except FileTooLargeToVersionError:
+        pass  # the on edit save will emit a warning
