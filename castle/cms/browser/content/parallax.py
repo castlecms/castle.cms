0830355ad35adae82268bf8a7d8f8779873a8cf5
David Henne <David@David-Hs-MacBook-Air.local>
2021-06-15 16:10:30 -0500

    Changes to properties and templates, separate parallax and static tiles, create various text options.

--- castle/cms/browser/content/parallax.py
+++ castle/cms/browser/content/parallax.py
@@ -16,18 +16,68 @@ class ParallaxView(BrowserView):
                 parsed = urlparse(tile_path)
                 data = parse_qs(parsed.query)
                 parallax_tile = {}
+
+                # Parallax tile properties
                 try:
                     image = uuidToObject(data['image:list'][0])
                     parallax_tile['image'] = image.absolute_url()
                 except Exception:
                     parallax_tile['image'] = None
 
+                parallax_tile['bg_color'] = data.get('bg_color', ['white'])[0]
                 parallax_tile['title'] = data.get('title', [None])[0]
                 parallax_tile['title_size'] = data.get('title_size', ['36px'])[0]
                 parallax_tile['text'] = data.get('text:text', [None])[0]
                 parallax_tile['text_size'] = data.get('text_size', ['12px'])[0]
-                parallax_tile['vert'] = data.get('vert_text_position', ['middle'])[0]
-                parallax_tile['hor'] = data.get('hor_text_position', ['center'])[0]
+                hor_position = data.get('hor_text_position', ['center'])[0]
+                if hor_position == 'start':
+                    parallax_tile['hor_left'] = '5%'
+                    parallax_tile['hor_right'] = '45%'
+                elif hor_position == 'end':
+                    parallax_tile['hor_left'] = '45%'
+                    parallax_tile['hor_right'] = '5%'
+                else:
+                    parallax_tile['hor_left'] = '10%'
+                    parallax_tile['hor_right'] = '10%'
+                parallax_tile['text_color'] = data.get('text_color', ['black'])[0]
+                shadow = data.get('text_shadow', [None])[0]
+                if shadow == 'black':
+                    parallax_tile['text_shadow'] = '1px 1px 1px black'
+                elif shadow == 'white':
+                    parallax_tile['text_shadow'] = '1px 1px 1px white'
+                else:
+                    parallax_tile['text_shadow'] = 'none'
+
+                # Static tile properties
+                try:
+                    image = uuidToObject(data['static_image:list'][0])
+                    parallax_tile['static_image'] = image.absolute_url()
+                except Exception:
+                    parallax_tile['static_image'] = None
+
+                parallax_tile['static_bg_color'] = data.get('static_bg_color', ['white'])[0]
+                parallax_tile['static_title'] = data.get('static_title', [None])[0]
+                parallax_tile['static_title_size'] = data.get('static_title_size', ['36px'])[0]
+                parallax_tile['static_text'] = data.get('static_text:text', [None])[0]
+                parallax_tile['static_text_size'] = data.get('static_text_size', ['12px'])[0]
+                hor_position = data.get('static_hor_text_position', ['center'])[0]
+                if hor_position == 'start':
+                    parallax_tile['static_hor_left'] = '5%'
+                    parallax_tile['static_hor_right'] = '45%'
+                elif hor_position == 'end':
+                    parallax_tile['static_hor_left'] = '45%'
+                    parallax_tile['static_hor_right'] = '5%'
+                else:
+                    parallax_tile['static_hor_left'] = '10%'
+                    parallax_tile['static_hor_right'] = '10%'
+                parallax_tile['static_text_color'] = data.get('static_text_color', ['black'])[0]
+                shadow = data.get('static_text_shadow', [None])[0]
+                if shadow == 'black':
+                    parallax_tile['static_text_shadow'] = '1px 1px 1px black'
+                elif shadow == 'white':
+                    parallax_tile['static_text_shadow'] = '1px 1px 1px white'
+                else:
+                    parallax_tile['static_text_shadow'] = 'none'
 
                 parallax_tiles.append(parallax_tile)
 
--- castle/cms/tiles/parallax.py
+++ castle/cms/tiles/parallax.py
@@ -22,85 +22,81 @@ class ParallaxTile(BaseTile):
 
 class IParallaxTileSchema(model.Schema):
 
-    # Type and Text
+    # --------------------- Parallax Tile ---------------------
     model.fieldset(
-        'type_and_text',
-        label=u'Type & Text',
+        'parallax_tile',
+        label=u'Parallax Tile',
         fields=[
-            'display_type',
+            'image',
+            'bg_color',
             'title',
-            'title_size',
             'text',
-            'text_size',
+            'text_color',
+            'text_shadow',
+            'hor_text_position',
         ]
     )
 
-    display_type = schema.Choice(
-        title=u"Display Type",
-        vocabulary=SimpleVocabulary([
-            SimpleTerm('background-image', 'background-image', u'Background Image'),  # noqa
-            SimpleTerm('left-image-right-text','left-image-right-text', u'Left Image Right Text'),  # noqa
-            SimpleTerm('background-video', 'background-video', u'Background Video'),  # noqa
-            SimpleTerm('left-video-right-text','left-video-right-text', u'Left Video Right Text'),  # noqa
-            SimpleTerm('resource-slide','resource-slide', u'Resource Slide')  # noqa
-        ]),
+    form.widget(image=ImageRelatedItemFieldWidget)
+    image = schema.List(
+        title=u'Parallax Image',
+        description=u'Image with scrolling parallax effect.',
+        required=False,
+        default=[],
+        value_type=schema.Choice(
+            vocabulary='plone.app.vocabularies.Catalog'
+        )
+    )
+
+    bg_color = schema.Choice(
+        title=u'Background Color',
+        description=u'If no image selected, tile will be this color',
         required=True,
-        default='background-image'
+        vocabulary=SimpleVocabulary([
+            SimpleTerm('black', 'black', u'Black'),
+            SimpleTerm('white', 'white', u'White'),
+        ]),
+        default=u'white'
     )
 
     title = schema.TextLine(
         title=u'Slide Title',
         description=u'Will be omitted if blank',
+        required=False,
         default=u'')
-
-    title_size = schema.Choice(
-        title=u'Title Size',
-        description=u'',
-        default=u'36px',
-        vocabulary=SimpleVocabulary([
-            SimpleTerm('36px', 'h1', 'h1'),
-            SimpleTerm('30px', 'h2', 'h2'),
-            SimpleTerm('24px', 'h3', 'h3'),
-            SimpleTerm('18px', 'h4', 'h4'),
-            SimpleTerm('14px', 'h5', 'h5'),
-            SimpleTerm('12px', 'h6', 'h6'),
-        ])
-    )
 
     text = schema.Text(
         title=u'Slide Text',
         description=u'Will be omitted if blank',
+        required=False,
         default=u'')
 
-    text_size = schema.Choice(
-        title=u'Text Size',
-        description=u'',
-        default=u'12px',
+    text_color = schema.Choice(
+        title=u'Text Color',
+        required=True,
         vocabulary=SimpleVocabulary([
-            SimpleTerm('20px', '20px', '20px'),
-            SimpleTerm('18px', '18px', '18px'),
-            SimpleTerm('16px', '16px', '16px'),
-            SimpleTerm('14px', '14px', '14px'),
-            SimpleTerm('12px', '12px', '12px'),
-            SimpleTerm('10px', '10px', '10px'),
-        ])
+            SimpleTerm('black', 'black', u'Black'),
+            SimpleTerm('white', 'white', u'White'),
+        ]),
+        default=u'black'
     )
 
-    # Text Positioning
-    model.fieldset(
-        'text_positioning',
-        label=u'Text Positioning',
-        fields=[
-            'hor_text_position',
-            'vert_text_position',
-            'justify_wrapped_text',
-        ]
+    text_shadow = schema.Choice(
+        title=u'Text Shadow',
+        description=u'Adds a shadow to the text over the image.',
+        required=True,
+        vocabulary=SimpleVocabulary([
+            SimpleTerm('None', 'None', u'None'),
+            SimpleTerm('black', 'black', u'Black'),
+            SimpleTerm('white', 'white', u'White'),
+        ]),
+        default=u'None'
     )
 
     hor_text_position = schema.Choice(
-        title=u"Slide Text Position (Horizontal)",
-        description=u'This setting only applies to large screens. '
-                    u'On small screens, Center (Full Width) will always be deisplayed.',
+        title=u'Slide Text Position (Horizontal)',
+        description=u'How the text will be horizontally positioned on the tile.',
+        required=True,
         vocabulary=SimpleVocabulary([
             SimpleTerm('start', 'start', u'Left (Half Width)'),
             SimpleTerm('center', 'center', u'Center (Full Width)'),
@@ -109,91 +105,84 @@ class IParallaxTileSchema(model.Schema):
         default=u'center'
     )
 
-    vert_text_position = schema.Choice(
-        title=u"Slide Text Position (Vertical)",
-        vocabulary=SimpleVocabulary([
-            SimpleTerm('top', 'top', u'Top'),
-            SimpleTerm('middle', 'middle', u'Middle'),
-            SimpleTerm('bottom', 'bottom', u'Bottom'),
-        ]),
-        default=u'middle'
-    )
-
-    justify_wrapped_text = schema.Bool(
-        title=u'Justify Wrapped Text Lines',
-        description=u'Select this option to force any text line that wraps to more than one line to take up the entire width. ' # noqa
-                    u'This will assure that there are no gaps to the left and/or right when text fills the whole line, regardless of Text Alignment Choice above.', # noqa 
-        default=False,
-    )
-
-    # Media Settings
+    # --------------------- Static Tile ---------------------
     model.fieldset(
-        'media_settings',
-        label=u'Media Settings',
+        'static_tile',
+        label=u'Static Tile',
         fields=[
-            'image',
+            'static_image',
+            'static_bg_color',
+            'static_title',
+            'static_text',
+            'static_text_color',
+            'static_text_shadow',
+            'static_hor_text_position',
         ]
     )
 
-    form.widget(image=ImageRelatedItemFieldWidget)
-    image = schema.List(
-        title=u"Slide Image",
-        description=u"Reference image on the site.",
-        required=True,
+    form.widget(static_image=ImageRelatedItemFieldWidget)
+    static_image = schema.List(
+        title=u'Slide Image',
+        description=u'Image on static tile to create parallax effect.',
+        required=False,
         default=[],
         value_type=schema.Choice(
             vocabulary='plone.app.vocabularies.Catalog'
         )
     )
 
-    # @invariant
-    # def validate_image(data):
-    #     if data.image and len(data.image) != 1:
-    #         raise Invalid("Must select 1 image")
-    #     if data.image:
-    #         utils = getMultiAdapter((getSite(), getRequest()),
-    #                                 name="castle-utils")
-    #         obj = utils.get_object(data.image[0])
-    #         if not obj or obj.portal_type != 'Image':
-    #             raise Invalid('Must provide image file')
+    static_bg_color = schema.Choice(
+        title=u'Background Color',
+        description=u'If no image selected, tile will be this color',
+        required=True,
+        vocabulary=SimpleVocabulary([
+            SimpleTerm('black', 'black', u'Black'),
+            SimpleTerm('white', 'white', u'White'),
+        ]),
+        default=u'white'
+    )
 
-    # height = schema.Choice(
-    #     title=u'Height of parallax image',
-    #     description=u'Represent the height (in pixels) that the parallax image will be.',
-    #     required=True,
-    #     default=u'400px',
-    #     vocabulary=SimpleVocabulary([
-    #         SimpleTerm('100px', '100px', '100px'),
-    #         SimpleTerm('200px', '200px', '200px'),
-    #         SimpleTerm('300px', '300px', '300px'),
-    #         SimpleTerm('400px', '400px', '400px'),
-    #         SimpleTerm('500px', '500px', '500px'),
-    #         SimpleTerm('600px', '600px', '600px'),
-    #         SimpleTerm('700px', '700px', '700px'),
-    #         SimpleTerm('800px', '800px', '800px'),
-    #         SimpleTerm('900px', '900px', '900px'),
-    #         SimpleTerm('1000px', '1000px', '1000px'),
-    #     ])
-    # )
+    static_title = schema.TextLine(
+        title=u'Slide Title',
+        description=u'Will be omitted if blank',
+        required=False,
+        default=u'')
 
-    # heading_color = schema.Choice(
-    #     title=u'Heading Color',
-    #     description=u'Select between black or white for text color',
-    #     default=u'black',
-    #     required=True,
-    #     vocabulary=SimpleVocabulary([
-    #         SimpleTerm('black', 'black', 'Black'),
-    #         SimpleTerm('white', 'white', 'White'),
-    #     ])
-    # )
+    static_text = schema.Text(
+        title=u'Slide Text',
+        description=u'Will be omitted if blank',
+        required=False,
+        default=u'')
 
-    # body_color = schema.Choice(
-    #     title=u'Body Color',
-    #     description=u'Select between black or white for text color',
-    #     default=u'black',
-    #     required=True,
-    #     vocabulary=SimpleVocabulary([
-    #         SimpleTerm('black', 'black', 'Black'),
-    #         SimpleTerm('white', 'white', 'White'),
-    #     ])
-    # )
+    static_text_color = schema.Choice(
+        title=u'Text Color',
+        required=True,
+        vocabulary=SimpleVocabulary([
+            SimpleTerm('black', 'black', u'Black'),
+            SimpleTerm('white', 'white', u'White'),
+        ]),
+        default=u'black'
+    )
+
+    static_text_shadow = schema.Choice(
+        title=u'Text Shadow',
+        description=u'Adds a shadow to the text over the image.',
+        required=True,
+        vocabulary=SimpleVocabulary([
+            SimpleTerm('None', 'None', u'None'),
+            SimpleTerm('black', 'black', u'Black'),
+            SimpleTerm('white', 'white', u'White'),
+        ]),
+        default=u'None'
+    )
+
+    static_hor_text_position = schema.Choice(
+        title=u'Slide Text Position (Horizontal)',
+        description=u'How the text will be horizontally positioned on the tile.',
+        vocabulary=SimpleVocabulary([
+            SimpleTerm('start', 'start', u'Left (Half Width)'),
+            SimpleTerm('center', 'center', u'Center (Full Width)'),
+            SimpleTerm('end', 'end', u'Right (Half Width)'),
+        ]),
+        default=u'center'
+    )
