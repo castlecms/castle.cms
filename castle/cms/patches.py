b9506fb133be3dbda0926d02bc33aaceb8c74bca
David Henne <David@David-Hs-MacBook-Air.local>
2021-09-02 14:23:36 -0500

    clean up code

--- castle/cms/patches.py
+++ castle/cms/patches.py
@@ -16,40 +16,19 @@ from OFS.CopySupport import _cb_encode
 from OFS.CopySupport import eInvalid
 from OFS.CopySupport import eNoData
 from plone import api
+from plone.app.widgets.base import dict_merge
+from plone.app.widgets.utils import get_tinymce_options
+from plone.app.z3cform.widget import RichTextWidget
 from plone.keyring.interfaces import IKeyManager
 from plone.session import tktauth
 from plone.transformchain.interfaces import ITransform
 from Products.CMFPlone.CatalogTool import CatalogTool
+from Products.CMFPlone.resources import add_resource_on_request
 from ZODB.POSException import ConnectionStateError
 from zope.component import getGlobalSiteManager
 from zope.component import queryUtility
 from zope.event import notify
 from zope.interface import implementer
-
-
-#! Imports for rich text widget
-
-from Acquisition import ImplicitAcquisitionWrapper
-from lxml import etree
-from plone.app.textfield.value import RichTextValue
-from plone.app.widgets.base import TextareaWidget
-from plone.app.widgets.base import dict_merge
-from plone.app.widgets.utils import get_tinymce_options
-from plone.app.z3cform.utils import closest_content
-import json
-from plone.app.z3cform.widget import BaseWidget
-from plone.app.textfield.widget import RichTextWidget as patextfield_RichTextWidget
-from plone.app.z3cform.interfaces import IRichTextWidget
-from plone.registry.interfaces import IRegistry
-from Products.CMFCore.utils import getToolByName
-from Products.CMFPlone.interfaces import IEditingSchema
-from Products.CMFPlone.resources import add_resource_on_request
-from UserDict import UserDict
-from zope.component import getUtility
-from zope.interface import implementsOnly
-
-
-
 
 
 logger = logging.getLogger('castle.cms')
@@ -199,136 +178,25 @@ def version_overview(self):
     return [CASTLE_VERSION_STRING] + self._old_version_overview()
 
 
-class RichTextWidget(BaseWidget, patextfield_RichTextWidget):
-    _base = TextareaWidget
-    implementsOnly(IRichTextWidget)
-    pattern_options = BaseWidget.pattern_options.copy()
+def rich_text_args(self):
+    args = super(RichTextWidget, self)._base_args()
+    args['name'] = self.name
+    value = self.value and self.value.raw_encoded or ''
+    args['value'] = (self.request.get(
+        self.field.getName(), value)).decode('utf-8')
+    args.setdefault('pattern_options', {})
 
-    def __init__(self, *args, **kwargs):
-        super(RichTextWidget, self).__init__(*args, **kwargs)
-        if api.user.is_anonymous():
-            add_resource_on_request(self.request, 'mockup-patterns-tinymce-logged-out')
-        self._pattern = None
+    # displays tinymce editor for anonymous users
+    if api.user.is_anonymous():
+        add_resource_on_request(self.request, 'mockup-patterns-tinymce-logged-out')
 
-    def wrapped_context(self):
-        context = self.context
-        content = closest_content(context)
-        if context.__class__ == dict:
-            context = UserDict(self.context)
-        return ImplicitAcquisitionWrapper(context, content)
+    merged_options = dict_merge(get_tinymce_options(self.context,
+                                                    self.field,
+                                                    self.request),  # noqa
+                                args['pattern_options'])
+    args['pattern_options'] = merged_options
 
-    @property
-    def pattern(self):
-        """dynamically grab the actual pattern name so it will
-           work with custom visual editors"""
-        if self._pattern is None:
-            registry = getUtility(IRegistry)
-            try:
-                records = registry.forInterface(IEditingSchema, check=False,
-                                                prefix='plone')
-                default = records.default_editor.lower()
-                available = records.available_editors  #['TinyMCE', 'None']
-            except AttributeError:
-                default = 'tinymce'
-                available = ['TinyMCE']
-            tool = getToolByName(self.wrapped_context(), "portal_membership")
-            member = tool.getAuthenticatedMember()
-            editor = member.getProperty('wysiwyg_editor')
-            if editor in available:
-                return default
-            elif editor in ('None', None):
-                return default
-            return default
-        return self._pattern
-
-    def _base_args(self):
-        args = super(RichTextWidget, self)._base_args()
-        args['name'] = self.name
-        value = self.value and self.value.raw_encoded or ''
-        args['value'] = (self.request.get(
-            self.field.getName(), value)).decode('utf-8')
-
-        args.setdefault('pattern_options', {})
-        merged_options = dict_merge(get_tinymce_options(self.context,
-                                                        self.field,
-                                                        self.request),  # noqa
-                                    args['pattern_options'])
-        args['pattern_options'] = merged_options
-
-        return args
-
-    def render(self):
-        """Render widget.
-        :returns: Widget's HTML.
-        :rtype: string
-        """
-        if self.mode != 'display':
-            # MODE "INPUT"
-            rendered = ''
-            allowed_mime_types = self.allowedMimeTypes()
-            if not allowed_mime_types or len(allowed_mime_types) <= 1:
-                # Display textarea with default widget
-                rendered = super(RichTextWidget, self).render()
-            else:
-                # Let pat-textarea-mimetype-selector choose the widget
-
-                # Initialize the widget without a pattern
-                base_args = self._base_args()
-                pattern_options = base_args['pattern_options']
-                del base_args['pattern']
-                del base_args['pattern_options']
-                textarea_widget = self._base(None, None, **base_args)
-                textarea_widget.klass = ''
-                mt_pattern_name = '{}{}'.format(
-                    self._base._klass_prefix,
-                    'textareamimetypeselector'
-                )
-
-                # Initialize mimetype selector pattern
-                # TODO: default_mime_type returns 'text/html', regardless of
-                # settings. fix in plone.app.textfield
-                value_mime_type = self.value.mimeType if self.value\
-                    else self.field.default_mime_type
-                mt_select = etree.Element('select')
-                mt_select.attrib['id'] = '{}_text_format'.format(self.id)
-                mt_select.attrib['name'] = '{}.mimeType'.format(self.name)
-                mt_select.attrib['class'] = mt_pattern_name
-                mt_select.attrib['{}{}'.format('data-', mt_pattern_name)] =\
-                    json.dumps({
-                        'textareaName': self.name,
-                        'widgets': {
-                            'text/html': {  # TODO: currently, we only support
-                                            # richtext widget config for
-                                            # 'text/html', no other mimetypes.
-                                'pattern': self.pattern,
-                                'patternOptions': pattern_options
-                            }
-                        }
-                    })
-
-                # Create a list of allowed mime types
-                for mt in allowed_mime_types:
-                    opt = etree.Element('option')
-                    opt.attrib['value'] = mt
-                    if value_mime_type == mt:
-                        opt.attrib['selected'] = 'selected'
-                    opt.text = mt
-                    mt_select.append(opt)
-
-                # Render the combined widget
-                rendered = '{}\n{}'.format(
-                    textarea_widget.render(),
-                    etree.tostring(mt_select)
-                )
-            return rendered
-
-        if not self.value:
-            return ''
-
-        if isinstance(self.value, RichTextValue):
-            return self.value.output
-
-        return super(RichTextWidget, self).render()
+    return args
 
 
 # AsyncResult objects have a memory leak in them in Celery 4.2.1.
--- castle/cms/static/patterns/tinymce/anonymouspattern.js
+++ castle/cms/static/patterns/tinymce/anonymouspattern.js
@@ -1,22 +1,16 @@
 /* global tinymce, jQuery */
 
 (function($) { $(function() {
-    // require(['mockup-patterns-tinymce']);
-
     // manually load tinymce is user is not authenticated...
     if($('body.userrole-authenticated').size() === 0){
       require(['tinymce'], function(){
         $('.pat-textareamimetypeselector').each(function(){
-          // when using the textareamimetypeselector pattern, the textarea
-          // is the previous element
           var $tinymce = $(this).prev();
           // hide the textareamimetypeselector
           $(this).hide();
 
-          // we need an id for the tinymce instance
           var id = 1 + Math.floor(100000 * Math.random());
           $tinymce.attr('id', id);
-
           tinymce.init({
             selector: "#" + id
           });
