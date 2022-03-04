ef3fe1318cf87b97f2d3492676b7aa75d3c2b0cc
Brian Duncan <brian.duncan@wildcardcorp.com>
2022-03-04 08:13:45 -0600

    Bd/fix sticky footer (#519)
    * update sticky footer to handle missing urls
    
    * update history

--- castle/cms/tiles/sticky.py
+++ castle/cms/tiles/sticky.py
@@ -7,14 +7,24 @@ from plone.app.uuid.utils import uuidToCatalogBrain
 
 
 class StickyFooterTile(BaseTile):
+
     @property
     def internal_link_url(self):
-        if self.data.get('internal_link'):
+        internal_link = self.get_data('internal_link')
+        if internal_link:
             try:
-                brain = uuidToCatalogBrain(self.data['internal_link'][0])
-                return brain.getURL()
+                brain = uuidToCatalogBrain(internal_link[0])
+                if brain:
+                    return brain.getURL()
             except Exception:
                 pass
+
+    @property
+    def link_url(self):
+        return self.internal_link_url or self.get_data('external_link')
+
+    def get_data(self, key):
+        return (self.data or {}).get(key, None)
 
 
 class IStickyFooterTileSchema(Interface):
