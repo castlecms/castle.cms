08e85e854ae73a3f827d25c3e7b1841a44325f57
Joel Kleier <joel.kleier@wildcardcorp.com>
2022-08-16 14:44:59 -0500

    Back to development: 3.0.0b112

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     name='castle.cms',
     description='CastleCMS Plone distribution main package',
     long_description_content_type='text/x-rst',
-    version='3.0.0b111',
+    version='3.0.0b112.dev0',
     long_description='%s\n%s' % (
         read('README.rst'),
         read('HISTORY.rst')
