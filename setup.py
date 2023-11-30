0ac5a07dd0c574861320f0ab99d98b0dd5269966
Joel Kleier <joel.kleier@wildcardcorp.com>
2023-11-30 15:01:03 -0600

    Preparing release 4.0.0a1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     name='castle.cms',
     description='CastleCMS Plone distribution main package',
     long_description_content_type='text/x-rst',
-    version='4.0.0a1.dev0',
+    version='4.0.0a1',
     long_description='%s\n%s' % (
         read('README.rst'),
         read('CHANGES.md')
