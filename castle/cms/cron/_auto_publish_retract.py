c931da19c602e3a0e465741805a4ecbb3996a190
Adam McNevin <miknevinas@gmail.com>
2023-12-20 11:04:08 -1000

    Add auto-publish/auto-retract cron script

---
+++ castle/cms/cron/_auto_publish_retract.py
@@ -0,0 +1,78 @@
+
+from DateTime import DateTime
+from plone import api
+from Products.CMFPlone.interfaces.siteroot import IPloneSiteRoot
+from tendo import singleton
+from zope.component.hooks import setSite
+
+import logging
+import transaction
+
+
+logger = logging.getLogger('castle.cms')
+
+
+def run_query(query_params):
+
+    catalog = api.portal.get_tool('portal_catalog')
+    content = catalog(**query_params)
+    
+    to_state = 'published' if query_params.get('effective') else 'private'
+    for brain in content:
+        obj = brain.getObject()
+        api.content.transition(
+            obj=obj, 
+            to_state=to_state
+        )
+        obj.setModificationDate()
+        obj.reindexObject(idxs=['modified'])
+    transaction.commit()
+
+
+def set_queries(site):
+    setSite(site)
+    
+    # XXX: Needed to run locally, probably not necessary for live environment
+    # from AccessControl.SecurityManagement import newSecurityManager
+    # admin = api.user.get(username='admin')
+    # newSecurityManager(None, admin)
+
+    # publish pending content with an effective date within 30 minutes of cron run
+    start = DateTime() - (30.0 / (24 * 60))
+    end = DateTime()
+    publish_query = {
+        'effective': {
+            'query': (start, end),
+            'range': 'min:max'
+        },
+        'review_state': ['pending', 'private'],
+        'sort_on': 'modified',
+        'sort_order': 'reverse'
+    }
+    run_query(publish_query)
+
+    # retract published content with an expiration date older than cron run
+    retract_query = {
+        'expires': {
+            'query': DateTime(),
+            'range': 'max'
+        },
+        'review_state': 'published'
+    }
+    run_query(retract_query)
+
+
+def run(app):
+    singleton.SingleInstance('autopublish')
+
+    for oid in app.objectIds():  # noqa
+        obj = app[oid]  # noqa
+        if IPloneSiteRoot.providedBy(obj):
+            try:
+                set_queries(obj)
+            except Exception:
+                logger.error('Could not update content for %s' % oid, exc_info=True)
+
+
+if __name__ == '__main__':
+    run(app)  # noqa
