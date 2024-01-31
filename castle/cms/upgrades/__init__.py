8ecd6a1b6dbe1e83981be2cd4461fa87a40f556c
Brian Duncan <brian.duncan@wildcardcorp.com>
2024-01-31 14:00:59 -0600

    protect upgrade step (#586)

--- castle/cms/upgrades/__init__.py
+++ castle/cms/upgrades/__init__.py
@@ -102,10 +102,14 @@ def upgrade_3011(site, logger=CASTLE_LOGGER):
         logger.info('reindexing object_provides for portal_type ' + portal_type)
         # this could take a while
         for portal_type_brain in api.content.find(portal_type=portal_type):
-            content_object = portal_type_brain.getObject()
-            content_object.reindexObject(idxs=['object_provides'])
-            if ITemplate.providedBy(content_object):
-                itemplate_objects.append(itemplate_objects)
+            try:
+                content_object = portal_type_brain.getObject()
+                content_object.reindexObject(idxs=['object_provides'])
+                if ITemplate.providedBy(content_object):
+                    itemplate_objects.append(itemplate_objects)
+            except Exception:
+                logger.info('something weird happened with ' + repr(portal_type_brain))
+                continue
 
     itemplate_object_count = len(itemplate_objects)
     plural_suffix = '' if itemplate_object_count == 1 else 's'
