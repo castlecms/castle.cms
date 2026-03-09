a240d5abcc07be6742cfa0b97dfd1865f336cdda
Harper Nordin <harper.nordin@wildcardcorp.com>
2026-03-09 12:56:26 -0500

    fix: restricted anonymous traffic to login page

--- castle/cms/shield.py
+++ castle/cms/shield.py
@@ -4,16 +4,9 @@ from castle.cms.interfaces import ISecureLoginAllowedView
 from plone.registry.interfaces import IRegistry
 from zExceptions import Redirect
 from zope.component import queryUtility
-
 import plone.api as api
 
 SHIELD = constants.SHIELD
-
-_blacklisted_meta_types = (
-    'Image', 'File', 'Filesystem Image',
-    'Filesystem File', 'Stylesheets Registry', 'JavaScripts Registry',
-    'DirectoryViewSurrogate', 'KSS Registry', 'Filesystem Directory View')
-
 
 def protect(req, recheck=False):
     url = req.getURL()
@@ -21,12 +14,20 @@ def protect(req, recheck=False):
     if '@@secure-login' in url.lower() and url != login_url:
         raise Redirect(login_url)
 
-    published = req.PARENTS[0]
-    mt = getattr(
-        getattr(published, 'aq_base', None),
-        'meta_type',
-        getattr(published, 'meta_type', None))
-    if mt in _blacklisted_meta_types or mt is None:
+    url = req.get('URL', None)
+    whitelisted_requests = (
+        'bootstrap.css',
+        'secure-login.css',
+        'secure-login.js',
+        'require.js',
+        'jquery.min.js',
+        'bootstrap.min.js',
+        'react.js',
+        'utils.js',
+        'favicon.ico',
+        )
+
+    if is_whitelisted(req, whitelisted_requests):
         return
 
     published = req.get('PUBLISHED')
@@ -70,3 +71,19 @@ Disallow: /"""
             anonymous = api.user.is_anonymous()
         if anonymous:
             raise Redirect(login_url)
+
+
+def is_whitelisted(request, whitelist):
+    url = request.get('URL', None)
+    if url is None:
+        return False
+
+    url_without_querys = url.split('?')[0]
+    for resource in whitelist:
+        if url_without_querys.endswith(resource):
+            return True
+
+    if '/@@site-logo' in url_without_querys:
+        return True
+
+    return False
